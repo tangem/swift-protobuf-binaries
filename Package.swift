bb1cf0deaa34d288ed74e7f47c5274915ed90213
Andrey Fedorov <m3g0byt3@gmail.com>
2024-02-01 19:41:22 +0300

    IOS-5792: Rename `SwiftProtobuf` -> `SwiftProtobufBinaries` to avoid ambiguity

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftProtobuf",
+    name: "SwiftProtobufBinaries",
     products: [
         .library(
             name: "SwiftProtobuf",
